19da78b3e7b140ddcee816ff20c2abd8a45ab47e
12412081 <bryllejeoff.ladic@benilde.edu.ph>
2025-11-06 12:15:17 +0800

    Added participant data collection

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,5 +1,5 @@
 print("Welcome to SMIT TechFest!")
-print("Event organized by [YOUR NAME HERE] of APPDAET [YOUR SECTION]")
+print("Event organized by Brylle Ladic of APPDAET BTCS2")
 print()
 
 num_participants = int(input("How many participants will register? "))
@@ -7,3 +7,20 @@ num_participants = int(input("How many participants will register? "))
 if num_participants <= 0:
     print("Invalid number of participants.")
 else:
+    participants = []
+
+    for i in range(num_participants):
+        name = input("Enter participant name: ")
+        track = input("Enter chosen track: ")
+
+        participant = {
+            "name": name,
+            "track": track
+        }
+        participants.append(participant)
+
+        print()
+        print("Registered Participants:")
+        for i in range(len(participants)):
+            print(str(i + 1) + ".", participants[i]["name"], "-", participants[i]["track"])
+
