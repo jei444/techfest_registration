89ae4982d2da07ada6a17745e5406d0ac02ef04e
12412081 <bryllejeoff.ladic@benilde.edu.ph>
2025-11-06 11:47:56 +0800

    implemented participant registration

---
+++ techfest_registration.py
@@ -0,0 +1,9 @@
+print("Welcome to SMIT TechFest!")
+print("Event organized by [YOUR NAME HERE] of APPDAET [YOUR SECTION]")
+print()
+
+num_participants = int(input("How many participants will register? "))
+
+if num_participants <= 0:
+    print("Invalid number of participants.")
+else:
