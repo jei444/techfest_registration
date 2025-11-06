e6a68be8d984ae5b819616c5bb616042f032280c
12412081 <bryllejeoff.ladic@benilde.edu.ph>
2025-11-06 12:45:03 +0800

    Added track summary report

--- techfest_registration.py
+++ techfest_registration.py
@@ -13,47 +13,58 @@ else:
         name = input("Enter participant name: ")
         track = input("Enter chosen track: ")
 
-        participant = {
-            "name": name,
-            "track": track
-        }
+        participant = {"name": name, "track": track}
         participants.append(participant)
 
-        print()
-        print("Registered Participants:")
-        for i in range(len(participants)):
-            print(str(i + 1) + ".", participants[i]["name"], "-", participants[i]["track"])
+    print()
+    print("Registered Participants:")
+    for i in range(len(participants)):
+        print(str(i + 1) + ".", participants[i]["name"], "-", participants[i]["track"])
 
-        print()
-        unique_tracks = set()
-        for participant in participants:
-            unique_tracks.add(participant["track"])
+    print()
+    unique_tracks = set()
+    for participant in participants:
+        unique_tracks.add(participant["track"])
 
-        print("Tracks offered in this event:")
-        track_list = list(unique_tracks)
-        tracks_output = ""
-        for i in range(len(track_list)):
-            if i == 0:
-                tracks_output = track_list[i]
-            else:
-                tracks_output = tracks_output + ", " + track_list[i]
-            print(tracks_output)
+    print("Tracks offered in this event:")
+    track_list = list(unique_tracks)
+    tracks_output = ""
+    for i in range(len(track_list)):
+        if i == 0:
+            tracks_output = track_list[i]
+        else:
+            tracks_output = tracks_output + ", " + track_list[i]
+    print(tracks_output)
 
-            print()
-            if len(unique_tracks) < 2:
-                print("Not enough variety in tracks.")
+    print()
+    if len(unique_tracks) < 2:
+        print("Not enough variety in tracks.")
 
-            print()
-            seen_names = set()
-            found_duplicate = False
+    print()
+    seen_names = set()
+    found_duplicate = False
 
-            for participant in participants:
-                name = participant["name"]
-                if name in seen_names:
-                    print("Duplicate participant name: ", name)
-                    found_duplicate = True
-                    break
-                seen_names.add(name)
+    for participant in participants:
+        name = participant["name"]
+        if name in seen_names:
+            print("Duplicate name found:", name)
+            found_duplicate = True
+            break
+        seen_names.add(name)
 
-            if found_duplicate == False:
-                print("No duplicate names.")
+    if found_duplicate == False:
+        print("No duplicate names.")
+
+    print()
+    track_summary = {}
+
+    for participant in participants:
+        track = participant["track"]
+        if track in track_summary:
+            track_summary[track] = track_summary[track] + 1
+        else:
+            track_summary[track] = 1
+
+    print("Participants per track:")
+    for track in track_summary:
+        print(track + ":", track_summary[track])
